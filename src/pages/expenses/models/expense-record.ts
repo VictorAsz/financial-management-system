908eda9477ef1a1359538a649c627ba0d87fe8ba
Victor Assunção <victor.a.schumann@gmail.com>
2024-09-14 01:29:59 -0300

    add: json-server

---
+++ src/pages/expenses/models/expense-record.ts
@@ -0,0 +1,14 @@
+export class ExpenseRecord{
+
+product: String = ""
+category: String = ""
+user: String = ""
+valor: String = ""
+purchaseDate: String = ""
+payDay: String = ""
+payMethod: String = ""
+status: string = ""
+installments: string = ""
+actualInstallments: string = ""
+
+} 
