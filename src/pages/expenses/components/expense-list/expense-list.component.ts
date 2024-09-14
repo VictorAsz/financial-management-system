8b9c7e0e5a0a1e7e22392a195dd57af7bf78b86f
Victor Assunção <victor.a.schumann@gmail.com>
2024-09-14 02:19:16 -0300

    add: expenses get method

--- src/pages/expenses/components/expense-list/expense-list.component.ts
+++ src/pages/expenses/components/expense-list/expense-list.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpensesService } from '../../services/expenses.service';
+import { ExpenseRecord } from '../../models/expense-record';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   standalone: true,
   selector: 'expense-list',
+  imports: [CommonModule],
   templateUrl: './expense-list.component.html',
   styleUrls: ['./expense-list.component.css']
 })
 export class ExpenseListComponent implements OnInit {
 
-  constructor() { }
+
+  public list: ExpenseRecord[] = []
+
+  constructor(
+    private _expensesService: ExpensesService
+  ) { }
 
   ngOnInit() {
+    this.getExpenses()
+    
   }
+
+
+  public getExpenses(){
+    this._expensesService.getExpenses().subscribe({
+      next: response => { 
+        this.list = response.records
+      },
+      error: () => {
+        console.log("erro GetExpense")
+      }
+
+    })
+  }
+
 
 }
---
+++ src/pages/expenses/models/ret-expense.ts
@@ -0,0 +1,9 @@
+import { ExpenseRecord } from "./expense-record"
+
+export class RetExpense {
+
+    error: boolean = false
+    errorMessage: string = ""
+    records: ExpenseRecord[] = []
+
+}
