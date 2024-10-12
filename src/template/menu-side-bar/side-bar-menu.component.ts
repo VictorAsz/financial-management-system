cc3333168cedce04e776dc7edb69d08a2afdea61
Victor Assunção <victor.a.schumann@gmail.com>
2024-10-12 11:53:48 -0300

    Feat: SideBar  collapse

--- src/template/menu-side-bar/side-bar-menu.component.ts
+++ src/template/menu-side-bar/side-bar-menu.component.ts
@@ -1,19 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import {
+  RouterLink,
+  RouterLinkActive,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 
 @Component({
   selector: 'app-menu-side-bar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, RouterModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    RouterModule,
+  ],
   templateUrl: './side-bar-menu.component.html',
-  styleUrls: ['./side-bar-menu.component.css']
+  styleUrls: ['./side-bar-menu.component.css'],
 })
 export class SideBarMenuComponent implements OnInit {
+  isCollapsed = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
   }
-
 }
