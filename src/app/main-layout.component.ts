cf3a1fb5d9cfb83f4c247cead9730807fe460f3e
Victor Assunção <victor.a.schumann@gmail.com>
2025-06-24 15:15:41 -0300

    feat: adiciona template padrão para as telas

---
+++ src/app/main-layout.component.ts
@@ -0,0 +1,40 @@
+
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../template/header/header.component';
+import { SideBarMenuComponent } from '../template/menu-side-bar/side-bar-menu.component';
+import { FooterComponent } from '../template/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    SideBarMenuComponent,
+    FooterComponent,
+  ],
+  providers: [],
+  template: `
+    <div class="flex min-h-screen flex-col">
+        <app-header></app-header>
+        <div class="flex flex-grow">
+            <app-menu-side-bar></app-menu-side-bar>
+            <div class="flex flex-col flex-grow">
+            <main class="flex-grow">
+                <router-outlet></router-outlet>
+            </main>
+            <footer class="w-full bg-gray-800 text-white flex justify-center items-center p-4">
+                <p>Made By Victor A.S</p>
+            </footer>
+            </div>
+        </div>
+    </div>
+
+  
+  `,
+})
+export class MainLayoutComponent {
+}
