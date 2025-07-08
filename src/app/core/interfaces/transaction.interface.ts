16f5f7cbf151dbcccab3964aed2ede9098e42a0f
Victor Assuncao <Victor.a.schumann@gmail.com>
2025-07-08 17:37:04 -0300

    feat: adiciona category e account a interface de transaction

--- src/app/core/interfaces/transaction.interface.ts
+++ src/app/core/interfaces/transaction.interface.ts
@@ -1,15 +1,21 @@
+import { Account } from "./account.interface";
+import { Category } from "./category.interface";
+
 export interface Transaction {
- id: number;
- user_id: number;
- title: string;
- total_amount: number;
- is_installment: boolean;
- installment_count: number;
- payment_method: string;
- account_id: number | null;
- destination_account_id: number | null;
- category_id: number | null;
- type: string;
- transaction_date: string; // ISO date string
- created_at: string; // ISO date string
+  id: number;
+  user_id: number;
+  title: string;
+  total_amount: number;
+  is_installment: boolean;
+  installment_count: number;
+  payment_method: string;
+  account_id: number | null;
+  destination_account_id: number | null;
+  category_id: number | null;
+  type: 'income' | 'expense' | 'transfer';
+  transaction_date: string;
+  created_at: string;
+
+  category?: Category;
+  account?: Account;
 }
